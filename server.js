b7274ce0e754b8a76b507e15b1d27121f3bb295e
Diego Sucaria <diego.m.sucaria@gds.ey.com>
2023-03-31 00:00:49 +0000

    added support for gpt-3.5-turbo and code syntax colorer

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static('public'));
 app.post('/generate', async (req, res) => {
     const prompt = req.body.prompt;
     const temperature = req.body.temperature;
+    const model = req.body.model;
     const openaiApiKey = process.env.OPENAI_API_KEY;
 
     const headers = {
@@ -23,16 +24,47 @@ app.post('/generate', async (req, res) => {
         'Authorization': `Bearer ${openaiApiKey}`,
     };
 
-    const data = {
-        prompt: prompt,
-        max_tokens: 1000,
-        n: 1,
-        stop: null,
-        temperature: temperature,
-    };
+    let data = ''
+    let url = ''
+
+    switch (model) {
+        case 'gpt-3.5-turbo':
+            url = 'https://api.openai.com/v1/chat/completions'
+            //convert text model prompt to gpt 3.5 messages
+            const regex = /(?:^|\n)(Human|AI):\s*(.*?)(?=(?:\n(?:Human|AI):\s*)|$)/gs;
+            let messages = [];
+
+            let match;
+            while ((match = regex.exec(prompt)) !== null) {
+                const role = match[1] === 'Human' ? 'user' : 'assistant';
+                const content = match[2];
+                messages.push({ role, content });
+            }
+
+            data = {
+                model: 'gpt-3.5-turbo',
+                messages: messages,
+                max_tokens: 2000,
+                n: 1,
+                stop: null,
+                temperature: temperature,
+            };
+            break;
+        case 'text-davinci-002':
+            url = 'https://api.openai.com/v1/engines/text-davinci-002/completions'
+
+            data = {
+                prompt: prompt,
+                max_tokens: 2000,
+                n: 1,
+                stop: null,
+                temperature: temperature,
+            };
+            break;
+    }
 
     try {
-        const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', data, { headers });
+        const response = await axios.post(url, data, { headers });
         res.json(response.data);
         // console.log(response.data)
         console.log(response.data.usage)
